d2bb207b5ef4ebc6f6cb968841b68bc71f14ab49
sorrycc <sorrycc@gmail.com>
2018-10-24 15:11:46 +0800

    update

--- .umirc.js
+++ .umirc.js
@@ -3,6 +3,6 @@ import { join } from 'path';
 export default {
   publicPath: '/test-gh-pages/',
   plugins: [
-    join(__dirname, '..', require('../package').main || 'index.js'),
+    'umi-plugin-gh-pages',
   ],
 }
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import styles from './index.css';
 export default function() {
   return (
     <div className={styles.normal}>
-      <h1>Page index</h1>
+      <h1>Page index from umi-plugin-gh-pages</h1>
     </div>
   );
 }
